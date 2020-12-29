80b34b7401fdf817ab20752ee765d2e80e07d6be
hansAGB <hans.aangeenbrug@icloud.com>
2020-12-29 21:59:36 +0100

    Everything works but project can't find mp3

--- EierWekker-iOS 14.3/ViewController.swift
+++ EierWekker-iOS 14.3/ViewController.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var updateProgress: UIProgressView!
     
-    
+    var player: AVAudioPlayer?
+    let audioSession = AVAudioSession.sharedInstance()
     
     let  kookDir: [String: Int] = [
         "Soft" : 3,
@@ -23,6 +25,16 @@ class ViewController: UIViewController {
     var timer = Timer()
     var totalTime = 60
     var timeCooked = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: .spokenAudio, options: .defaultToSpeaker)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     
 
     @IBAction func buttonPressed(_ sender: UIButton) {
@@ -52,7 +64,28 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleLabel.text = "KLAAR!"
+            playSound()
         }
+    }
+
+    func playSound() {
+       
+        let url = Bundle.main.url(forResource: "EierWekker-iOS 14.3/alarm_sound", withExtension: "mp3")
+        
+        do {
+            if url != nil {
+                player = try AVAudioPlayer(contentsOf: url!)
+            } else {
+                print("mp3 not found")
+                return
+            }
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        player!.prepareToPlay()
+        player!.volume=1.0
+        player!.play()
     }
     
 }
