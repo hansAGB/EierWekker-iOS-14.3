181d7c3f63238941a4e3bd3e4e88b20d78e928b7
hansAGB <hans.aangeenbrug@icloud.com>
2020-12-29 19:11:28 +0100

    Timer toegevoegd

--- EierWekker-iOS 14.3/ViewController.swift
+++ EierWekker-iOS 14.3/ViewController.swift
@@ -14,11 +14,41 @@ class ViewController: UIViewController {
         "Medium" : 8,
         "Hard" : 12
     ]
+    
+    var timer: Timer?
+    var count = 60
+    var timerRuns = false
+    
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        print("Kooktijd = ", kookDir[hardness]!)
+        if !timerRuns {
+            timerRuns = true
+            
+            print("Kooktijd = ", kookDir[hardness]!)
+            
+            // 60 x seconde is minuut
+            count = 60 * kookDir[hardness]!
+            
+            print("start")
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func update() {
+        if(count > 0) {
+            print(count, "seconds")
+            count = count - 1
+        } else {
+            if let timer = self.timer {
+                timer.invalidate()
+                self.timer = nil
+            }
+            print("end")
+            timerRuns = false
+        }
     }
     
 }
