4e124b87bf87c5a81d905871f999f87a2e4b60e5
hansAGB <hans.aangeenbrug@icloud.com>
2020-12-29 21:02:06 +0100

    Reset to progressBar add

--- EierWekker-iOS 14.3/ViewController.swift
+++ EierWekker-iOS 14.3/ViewController.swift
@@ -10,44 +10,33 @@ import UIKit
 class ViewController: UIViewController {
 
     let  kookDir: [String: Int] = [
-        "Soft" : 5,
-        "Medium" : 8,
-        "Hard" : 12
+        "Soft" : 3,
+        "Medium" : 4,
+        "Hard" : 7
     ]
     
-    var timer: Timer?
-    var count = 60
-    var timerRuns = false
+    var timer = Timer()
+    var totalTime = 60
+    var timeCooked = 0
     
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
+
+        print("Kooktijd = ", kookDir[hardness]!)
         
-        if !timerRuns {
-            timerRuns = true
-            
-            print("Kooktijd = ", kookDir[hardness]!)
-            
-            // 60 x seconde is minuut
-            count = 60 * kookDir[hardness]!
-            
-            print("start")
-            
-            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        timeCooked = 0
+        totalTime = kookDir[hardness]!
+        
+        print("start")
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
         }
-    }
+
     
     @objc func update() {
-        if(count > 0) {
-            print(count, "seconds")
-            count = count - 1
-        } else {
-            if let timer = self.timer {
-                timer.invalidate()
-                self.timer = nil
-            }
-            print("end")
-            timerRuns = false
+        if timeCooked > totalTime {
+             timeCooked += 1
         }
     }
     
